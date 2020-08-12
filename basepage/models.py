a6684b72d4142ee9d4bdfcc789ccb73eefe7eb26
Alex Shashko <alex.shashko00@gmail.com>
2020-08-12 20:59:35 +0300

    Add str methods in models

--- basepage/models.py
+++ basepage/models.py
@@ -2,7 +2,7 @@ import os
 from django.conf import settings
 
 from django.db.models import Model, CASCADE
-from django.db.models import CharField, FloatField, TextField, FilePathField, IntegerField, PositiveIntegerField
+from django.db.models import CharField, FloatField, TextField, FilePathField, PositiveIntegerField
 from django.db.models import ManyToManyField, ForeignKey
 
 
@@ -13,20 +13,32 @@ def images_path():
 class Option(Model):
     name = CharField(max_length=120, null=False)
 
+    def __str__(self):
+        return f"{self:id}: {self.name}"
+
 
 class OptionParameter(Model):
     name = CharField(max_length=120, null=False)
     option = ForeignKey(Option, on_delete=CASCADE)
 
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
 
 class Image(Model):
     src = FilePathField(path=images_path, null=False)
+
+    def __str__(self):
+        return f"{self.id}: {self.src}"
 
 
 class Category(Model):
     parent = ForeignKey("self", on_delete=CASCADE, null=True, blank=True)
     name = CharField(max_length=120, null=False)
     description = TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.parent} => {self.id}: {self.name}"
 
 
 class Product(Model):
@@ -53,8 +65,14 @@ class Product(Model):
         # self.quantity.editable = False
         super(Product, self).save(*args, **kwargs)
 
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
 
 class OptionProduct(Model):
     product = ForeignKey(Product, on_delete=CASCADE)
     option_parameter = ForeignKey(OptionParameter, on_delete=CASCADE)
     quantity = PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.option_parameter} in {self.product} = {self.quantity}"
