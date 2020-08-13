3047cace639d82d4b55c5fd0741c06405f151774
JackMoran88 <dmitriy.evseev.99@gmail.com>
2020-08-13 09:54:36 +0300

    доработка base, header

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // //Dropdown
-$(".dropdown__content").hide(),
+    $(".dropdown__content").hide(),
         $(".dropdown__btn").click(function () {
             $(this).siblings(".dropdown__content").slideToggle(),
                 $(this).children(".fa").toggleClass(["fa-caret-right", "fa-caret-down"])
@@ -8,3 +8,37 @@ $(".dropdown__content").hide(),
 // //!Dropdown
 
 });
+
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function (mutations, observer) {
+    // fired when a mutation occurs
+    // console.log(mutations, observer);
+    // ...
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+    subtree: true,
+    attributes: true,
+    childList: true,
+    // attributeFilter: 'html',
+});
+
+
+//Отступ для контента от Header
+$(document).ready(function () {
+    $(window).on('resize', function () {
+        var win = $(this); //this = window
+        $('div.wrapper').css({'padding-top': $('nav.navbar').outerHeight()})
+        if (win.width() <= 991){
+            $('div.navbar-collapse').css({'padding-top': $('nav.navbar').outerHeight()})
+        }
+        else{
+            $('div.navbar-collapse').css({'padding-top': 0})
+        }
+    });
+})
+//!Отступ для контента от Header
