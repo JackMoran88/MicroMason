cae171faab635fc0a8d8643a0c1b5606d1f9b9d1
Alex Shashko <alex.shashko00@gmail.com>
2020-08-13 18:37:12 +0300

    Add getting categories(2 lvls) in views

--- basepage/urls.py
+++ basepage/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.index),
-
-
 
     path('api/sign_up/', views.api_sign_up),
     path('api/sign_in/', views.api_sign_in)
