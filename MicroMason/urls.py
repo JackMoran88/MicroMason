0d5e8cd5b5c55be35f2d10c0671ff3af7b8b0b37
Alex Shashko <alex.shashko00@gmail.com>
2020-08-26 15:10:12 +0300

    Finish settings of DRF

--- MicroMason/urls.py
+++ MicroMason/urls.py
@@ -23,5 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('basepage.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('api/base/', include('basepage.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- basepage/urls.py
+++ basepage/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-
-    path('api/sign_up/', views.api_sign_up),
-    path('api/sign_in/', views.api_sign_in)
 ]
